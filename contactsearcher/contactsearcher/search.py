f5226c9a48a65b771000485717649509b596b769
TitusSmith33 <smith03@allegheny.edu>
2022-11-14 20:10:34 -0500

    Added functionality to the 2 functions

--- contactsearcher/contactsearcher/search.py
+++ contactsearcher/contactsearcher/search.py
@@ -2,18 +2,42 @@
 
 from typing import List
 
-import csv
+# import csv
 
-# note: see https://docs.python.org/3/library/csv.html 
+# note: see https://docs.python.org/3/library/csv.html
 
 
-def search_for_email_given_job(job_description: str, contacts: str) -> List[List[str]]:
-    """Search for and return job description(s) given an email address."""
-    # TODO: create an empty list of the contacts
-    # TODO: iterate through the file, parsing it line by line
-    # TODO: refer to the file called input/contacts.txt to learn more about
-    # the format of the comma separated value (CSV) file that we must parse
-    # TODO: iterate through each line of the file and extract the current job
-    # ---> TODO: extract the current job for the contact on this line of the CSV
-    # ---> TODO: the job description matches and thus we should save it in the list
-    # TODO: return the list of the contacts who have a job description that matches
+def search_for_email_given_job(job_description: str, contacts: str) -> List[str]:
+    """Search for and return job description(s)."""
+    # Create an empty list of the jobs
+    job_list = []
+    # job_list = []
+    # iterate through the file, parsing it line by line
+    for line in contacts.splitlines():
+        # email_str = line[: line.find(",")]
+        # find the job for each person
+        job_str = line[line.find(",") + 1 :].replace('"', "")
+        # if the job you are looking for is in the job title execute this code
+        if job_description.lower() in job_str.lower():
+            # add the job and email to the list
+            job_list.append(job_str)
+    # return the list with job information
+    return job_list
+
+
+def find_email(job_description: str, contacts: str) -> List[str]:
+    """Find the email correspoding to the jpb we are looking for."""
+    # create an empty list for the emails
+    email_list = []
+    # iterate through the csv
+    for line in contacts.splitlines():
+        # find the email per line
+        email_str = line[: line.find(",")]
+        # find the job per line
+        job_str = line[line.find(",") + 1 :].replace('"', "")
+        # if the job you are looking for is in the job title execute this code
+        if job_description.lower() in job_str.lower():
+            # add the email to the list
+            email_list.append(email_str)
+    # return the list of emails with the index corresponding to the job list
+    return email_list
